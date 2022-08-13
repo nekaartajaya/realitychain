7893af9508f519a890122777e83a5025917c36e1
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-08-14 06:02:19 +0700

    chore: deploy and build scripts for mock ft

---
+++ packages/realitychain-contracts/mock-ft/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+NEAR_ENV=testnet near dev-deploy --wasmFile res/fungible_token.wasm --helperUrl https://near-contract-helper.onrender.com
+
+input="./neardev/dev-account"
+cat "$input" > "../../../fungible-token-address"
---
+++ packages/realitychain-contracts/mock-ft/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ft_input="../../../fungible-token-address"
+ft_acc=$( cat "$ft_input" )
+
+params="{\"owner_id\": \"$ft_acc\", \"total_supply\": \"1000000000000000000000000\", \"metadata\": { \"spec\": \"ft-1.0.0\", \"name\": \"Real FT Token V2.1\", \"symbol\": \"REAL21\", \"decimals\": 8 }}"
+
+near call $ft_acc new "'$params'" --accountId $ft_acc
+near call $ft_acc storage_deposit '' --accountId agustinustheo.testnet --amount 0.00125
+
+params2="{\"receiver_id\": \"agustinustheo.testnet\", \"amount\": \"100000000000000000000000\"}"
+
+near call $ft_acc ft_transfer "'$params2'" --accountId $ft_acc --amount 0.000000000000000000000001
