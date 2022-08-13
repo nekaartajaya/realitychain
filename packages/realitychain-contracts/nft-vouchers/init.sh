d4cf12622b3da38ca268391d1b9d09606fec9f3e
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-08-14 06:08:38 +0700

    chore: add configurable owner to script

--- packages/realitychain-contracts/nft-vouchers/init.sh
+++ packages/realitychain-contracts/nft-vouchers/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+owner_input="../../../owner-address"
+owner_acc=$( cat "$owner_input" )
+
 ft_input="../../../fungible-token-address"
 ft_acc=$( cat "$ft_input" )
 
@@ -8,6 +11,6 @@ parcel_acc=$( cat "$parcel_input" )
 voucher_input="../../../nft-voucher-address"
 voucher_acc=$( cat "$voucher_input" )
 
-params="{\"voucher_nft_id\": \"$voucher_acc\", \"parcel_nft_id\": \"$parcel_acc\", \"real_ft_id\": \"$ft_acc\", \"owner_id\": \"agustinustheo.testnet\", \"treasury_id\":\"agustinustheo.testnet\"}"
+params="{\"voucher_nft_id\": \"$voucher_acc\", \"parcel_nft_id\": \"$parcel_acc\", \"real_ft_id\": \"$ft_acc\", \"owner_id\": \"$owner_acc\", \"treasury_id\":\"$owner_acc\"}"
 
-near call $voucher_acc new_default_meta "'$params'" --accountId agustinustheo.testnet
+near call $voucher_acc new_default_meta "'$params'" --accountId $owner_acc
