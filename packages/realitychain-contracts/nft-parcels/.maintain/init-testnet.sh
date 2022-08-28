f5f5abe667c94efcba2b7e52d55e5c93208dcafc
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-08-29 03:05:06 +0700

    chore: init testnet script

---
+++ packages/realitychain-contracts/nft-parcels/.maintain/init-testnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+owner_input="../../../../addresses/testnet/owner-address"
+owner_acc=$( cat "$owner_input" )
+
+ft_input="../../../../addresses/testnet/fungible-token-address"
+ft_acc=$( cat "$ft_input" )
+
+parcel_input="../../../../addresses/testnet/nft-parcel-address"
+parcel_acc=$( cat "$parcel_input" )
+
+params="{\"parcel_nft_id\": \"$parcel_acc\", \"real_ft_id\": \"$ft_acc\", \"owner_id\": \"$owner_acc\", \"treasury_id\":\"$owner_acc\"}"
+
+near call $parcel_acc new_default_meta "'$params'" --accountId $owner_acc
+
+near call $ft_acc storage_deposit '' --accountId $parcel_acc --amount 0.00125
